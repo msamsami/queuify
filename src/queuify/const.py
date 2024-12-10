6104a88e52777e02862aa65df50feec77de70fcc
Mehdi Samsami <mehdisamsami@live.com>
2024-12-10 19:21:16 +0330

    add constant variables, add typing marker file

---
+++ src/queuify/const.py
@@ -0,0 +1,9 @@
+QUEUE_DELETION_MSG = "_qd_"
+
+NO_REMAINING_TASK_MSG = "_nrt_"
+
+SEMAPHORE_TOKEN = "_t_"
+
+TASK_DONE_TOO_MANY_TIMES_ERROR_MSG = "task_done() called too many times"
+
+TIMEOUT_NEGATIVE_ERROR_MSG = "'timeout' must be a non-negative number"
